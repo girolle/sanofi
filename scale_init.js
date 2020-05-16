0b270aa94307aab6043d632619f84a07c1d52e26
Girolle <khas98@gmail.com>
2020-05-16 18:19:44 +0300

    first

---
+++ scale_init.js
@@ -0,0 +1,18 @@
+var windowHeight = 0.99 * document.documentElement.clientHeight;
+var windowWidth = 0.99 * document.documentElement.clientWidth;
+
+var quizWidth;
+if (windowWidth > 1000) {quizWidth =  "950px";}
+else {quizWidth = (0.95 * windowWidth) + "px";}
+
+var h1Size;
+if (parseInt(quizWidth) / 15 > 30 ) {h1Size = 30 + "px";}
+else {h1Size = (parseInt(quizWidth) / 20) + "px";}
+
+for (let j = 0; j <=8; j+=1) {
+	$("#q" + j)[0].style.width = quizWidth;
+	$("#a" + j)[0].style.width = quizWidth;
+
+}
+
+$("#finish")[0].style.width = quizWidth;
