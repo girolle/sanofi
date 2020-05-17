697841678d15d1796e4967487cb3201814e697ca
Girolle <khas98@gmail.com>
2020-05-17 20:47:22 +0300

    new design

--- scale_init.js
+++ scale_init.js
@@ -2,8 +2,8 @@ var windowHeight = 0.99 * document.documentElement.clientHeight;
 var windowWidth = 0.99 * document.documentElement.clientWidth;
 
 var quizWidth;
-if (windowWidth > 1000) {quizWidth =  "950px";}
-else {quizWidth = (0.95 * windowWidth) + "px";}
+if (windowWidth > 1000) {quizWidth =  "1000px";}
+else {quizWidth = (windowWidth) + "px";}
 
 var h1Size;
 if (parseInt(quizWidth) / 15 > 30 ) {h1Size = 30 + "px";}
